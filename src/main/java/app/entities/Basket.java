c1854d05b28d42e8af7e94ddcff78af198ad2f05
sofusk7cdk <sofus@k7c.dk>
2025-10-21 10:04:33 +0200

    basket dto and basket entity

--- src/main/java/app/entities/Basket.java
+++ src/main/java/app/entities/Basket.java
@@ -1,4 +1,34 @@
 package app.entities;
 
+import app.dto.BasketDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Basket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToMany(cascade = CascadeType.PERSIST)
+    List<Product> products = new ArrayList<>();
+
+    public Basket(int id, List<Product> products) {
+        this.id = id;
+        this.products = products;
+    }
+
+    public Basket(BasketDTO basketDTO) {
+        this.id = basketDTO.getId();
+        this.products = basketDTO.getProducts();
+    }
 }
