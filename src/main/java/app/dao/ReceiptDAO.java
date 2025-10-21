49ffb16c4f4816e30eff33f0e9975eed64aab98c
KnogleMau <mav.solver@hotmail.com>
2025-10-21 22:55:39 +0200

    Næsten færdig med mine DAO klasse, implamenter controller og routes imorgen.
    Laver test til min dao imorgen også

--- src/main/java/app/dao/ReceiptDAO.java
+++ src/main/java/app/dao/ReceiptDAO.java
@@ -1,4 +1,62 @@
 package app.dao;
 
-public class ReceiptDAO {
+import app.dto.ReceiptDTO;
+import app.entities.Basket;
+import app.entities.Receipt;
+import app.exceptions.ApiException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+public class ReceiptDAO implements IDAO<Receipt, ReceiptDTO> {
+    EntityManagerFactory emf;
+
+    public ReceiptDAO(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    @Override
+    public Receipt create(ReceiptDTO receiptDTO) {
+        Receipt receipt = new Receipt(receiptDTO);
+        try(EntityManager em = emf.createEntityManager()){
+            em.getTransaction().begin();
+            em.persist(receipt);
+            em.getTransaction().commit();
+        }
+        catch(ApiException e){
+            throw new ApiException(e.getCode(), "Failed to save receipt to the database");
+        }
+        return receipt;
+    }
+
+    @Override
+    public Receipt findById(int id) {
+        try(EntityManager em = emf.createEntityManager()){
+            em.getTransaction().begin();
+            Receipt foundReceipt = em.find(Receipt.class, id);
+            em.getTransaction().commit();
+            return foundReceipt;
+        } catch(ApiException e){
+            throw new ApiException(e.getCode(), "Failed to find receipt with id " + id);
+        }
+
+    }
+
+    @Override
+    public Receipt update(ReceiptDTO receiptDTO, Integer id) {
+        try(EntityManager em = emf.createEntityManager()){
+            em.getTransaction().begin();
+            Receipt updatedReceipt = em.find(Receipt.class, id);
+            if(updatedReceipt != null){
+                updatedReceipt.setTotalPrice(receiptDTO.getTotalPrice());
+                em.merge(updatedReceipt);
+                em.getTransaction().commit();
+            }
+return updatedReceipt;
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
