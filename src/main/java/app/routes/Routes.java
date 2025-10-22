078cff5b854650283f7b861c1b0696357f9ef2dd
KnogleMau <mav.solver@hotmail.com>
2025-10-22 21:41:36 +0200

    Lige lavet mine http request

--- src/main/java/app/routes/Routes.java
+++ src/main/java/app/routes/Routes.java
@@ -1,4 +1,16 @@
 package app.routes;
 
+import io.javalin.apibuilder.EndpointGroup;
+
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.path;
+
 public class Routes {
+    RecieptRoutes recieptRoutes = new RecieptRoutes();
+    public EndpointGroup getRoutes(){
+        return () -> {
+            get("/", ctx -> ctx.result("Hello World"));
+            path("/receipt", recieptRoutes.getRoutes());
+        };
+    }
 }
