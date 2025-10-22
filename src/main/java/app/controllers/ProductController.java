069dda521e6afa456a090fa10f55cf1545355cb5
NicolaiKristiansen <nicolaikristiansen223@gmail.com>
2025-10-22 14:53:37 +0200

    + Made the ProductController class + Finished ProductDAO class + Made test for the DAO methods + Made a controller for products + Made routes for the products + Fix a syntax error

--- src/main/java/app/controllers/ProductController.java
+++ src/main/java/app/controllers/ProductController.java
@@ -24,7 +24,6 @@ public class ProductController {
                 ctx.status(HttpStatus.OK).json(createdProduct);
             } catch (Exception e) {
                 e.printStackTrace();
-                e.
             }
         }
 
