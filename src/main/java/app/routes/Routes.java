af8aef46690f1eebaf0a95c60e1897f459953f95
sofusk7cdk <sofus@k7c.dk>
2025-10-23 09:39:18 +0200

    product routes

--- src/main/java/app/routes/Routes.java
+++ src/main/java/app/routes/Routes.java
@@ -6,16 +6,16 @@ import static io.javalin.apibuilder.ApiBuilder.get;
 import static io.javalin.apibuilder.ApiBuilder.path;
 
 public class Routes {
-
   private BasketRoutes basketRoutes = new BasketRoutes();
   private RecieptRoutes recieptRoutes = new RecieptRoutes();
+  private ProductRoutes productRoutes = new ProductRoutes();
 
-    public EndpointGroup getRoutes() {
+  public EndpointGroup getRoutes() {
         return () -> {
             get("/", ctx -> ctx.result("Hello World"));
             path("/baskets", basketRoutes.getRoutes());
-          path("/receipt", recieptRoutes.getRoutes());
-  
+            path("/receipt", recieptRoutes.getRoutes());
+            path("/products", productRoutes.getRoutes());
         };
     }
 }
