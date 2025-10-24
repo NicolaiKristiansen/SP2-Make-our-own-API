c67b290a72ce29bd471241ed6cbbbb9e50264e80
sofusk7cdk <sofus@k7c.dk>
2025-10-24 13:39:53 +0200

    Main refactor

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,18 +3,15 @@ package app;
 import app.config.ApplicationConfig;
 import app.config.HibernateConfig;
 import app.routes.Routes;
-import jakarta.persistence.EntityManagerFactory;
 
 import static io.javalin.apibuilder.ApiBuilder.get;
 import static io.javalin.apibuilder.ApiBuilder.path;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello SP2");
 
-        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+        HibernateConfig.getEntityManagerFactory();
 
         ApplicationConfig
                 .getInstance()
